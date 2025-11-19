3f00a4184e3452daf33f45598fc7fb8a4a36d494
hsa-01 <hafed.sassi.01@gmail.com>
2025-11-19 11:57:25 +0100

    Add files via upload
    Création initiale.

---
+++ pipeline_etl/fonc_airport_rating.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def airport_rating(df_data_prov, airport_rating_csv):
+    '''
+    PURPOSE : 
+        This file is a function called by the main.py file. Its purpose is to enrich dataset with ponctuality ratings of the departure and arrival airports from internal source
+        (csv from AirHelp)
+    ARGS:
+        df_data_prov(df) : Data frame temporary of the pipeline ETL
+        airport_rating_csv (csv) : Internal source 
+    RETURNS:
+        float: Ponctuality ratings of the departure and arrival airports     
+    '''
+
+    try:
+
+        #=============================
+        # PART 1 : RATING of DEPARTURE AIRPORT
+        #=============================
+        # MAPPING DEFINITION : Creation of a mapping dict for a direct approach
+        rating_mapping = airport_rating_csv.set_index('IATA_airport_code')['ponctuality_rating'].to_dict()
+        
+        # MAPPING APPLICATION : Apply the mapping only on NaN values 
+        mask = df_data_prov['ds_departure_airport_rating'].isna()
+        df_data_prov.loc[mask, 'ds_departure_airport_rating'] = df_data_prov.loc[mask, 'ds_departure_airport_code'].map(rating_mapping)
+
+
+        df_data_prov['ds_departure_airport_rating'] = (
+        df_data_prov['ds_departure_airport_rating']
+        .astype(str)                     # Ensuring that is text
+        .str.replace(',', '.', regex=True)  
+        .astype(float)                   # Conversion in float
+    )
+
+
+        #=============================
+        # PART 2 : RATING of ARRIVAL AIRPORT
+        #=============================
+        # MAPPING DEFINITION : Creation of a mapping dict for a direct approach
+        rating_mapping = airport_rating_csv.set_index('IATA_airport_code')['ponctuality_rating'].to_dict()
+        
+        # MAPPING APPLICATION : Apply the mapping only on NaN values 
+        mask = df_data_prov['ds_arrival_airport_rating'].isna()
+        df_data_prov.loc[mask, 'ds_arrival_airport_rating'] = df_data_prov.loc[mask, 'ds_arrival_airport_code'].map(rating_mapping)
+
+
+        df_data_prov['ds_arrival_airport_rating'] = (
+        df_data_prov['ds_arrival_airport_rating']
+        .astype(str)                     # Ensuring that is text
+        .str.replace(',', '.', regex=True)  
+        .astype(float)                   # Conversion in float
+    )
+
+        return df_data_prov
+
+    except Exception as e:
+        print(f"Erreur dans aiport_rating: {e}")
+        return np.nan
