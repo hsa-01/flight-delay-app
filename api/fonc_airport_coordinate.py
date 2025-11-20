e5f0c0e08bf8264d1ff3e5f9a40837a4b09da306
hsa-01 <hafed.sassi.01@gmail.com>
2025-11-20 11:20:12 +0100

    Add files via upload
    Création initiale.

---
+++ api/fonc_airport_coordinate.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def airport_coordinate(df_data_prov, airport_coord_csv):
+    '''
+    PURPOSE : 
+        This file is a function called by the main.py file. Its purpose is to enrich dataset with coordinates of the departure and arrival airports from internal source
+        (csv from OurAirports)
+    ARGS:
+        df_data_prov (df) : Data frame temporary of the pipeline ETL
+        airport_coord_csv (csv) : Internal source 
+    RETURNS:
+        float: Coodinates (LONG,LAT) of the departure and arrival airports     
+    '''
+
+
+    try:
+
+        #=============================
+        # PART 1 : LATITUTE DEPARTURE
+        #=============================
+        # MAPPING DEFINITION : Creation of a mapping dict for a direct approach
+        coord_mapping = airport_coord_csv.set_index('IATA')['GeoPointLat'].to_dict()
+        
+        # MAPPING APPLICATION : Apply the mapping only on NaN values 
+        mask = df_data_prov['ds_departure_airport_lat'].isna()
+        df_data_prov.loc[mask, 'ds_departure_airport_lat'] = df_data_prov.loc[mask, 'ds_departure_airport_code'].map(coord_mapping)
+        
+        
+        #=============================
+        # PART 2 : LONGITUDE DEPARTURE
+        #=============================
+        # MAPPING DEFINITION : Creation of a mapping dict for a direct approach
+        coord_mapping = airport_coord_csv.set_index('IATA')['GeoPointLong'].to_dict()
+        
+        # MAPPING APPLICATION : Apply the mapping only on NaN values 
+        mask = df_data_prov['ds_departure_airport_long'].isna()
+        df_data_prov.loc[mask, 'ds_departure_airport_long'] = df_data_prov.loc[mask, 'ds_departure_airport_code'].map(coord_mapping)
+        
+        #=============================
+        # PART 3 : LATITUDE ARRIVAL
+        #=============================
+        # MAPPING DEFINITION : Creation of a mapping dict for a direct approach
+        coord_mapping = airport_coord_csv.set_index('IATA')['GeoPointLat'].to_dict()
+        
+        # MAPPING APPLICATION : Apply the mapping only on NaN values 
+        mask = df_data_prov['ds_arrival_airport_lat'].isna()
+        df_data_prov.loc[mask, 'ds_arrival_airport_lat'] = df_data_prov.loc[mask, 'ds_arrival_airport_code'].map(coord_mapping)
+        
+        
+        #=============================
+        # PART 4 : LONGITUDE ARRIVAL
+        #=============================
+        # MAPPING DEFINITION : Creation of a mapping dict for a direct approach
+        coord_mapping = airport_coord_csv.set_index('IATA')['GeoPointLong'].to_dict()
+        
+        # MAPPING APPLICATION : Apply the mapping only on NaN values 
+        mask = df_data_prov['ds_arrival_airport_long'].isna()
+        df_data_prov.loc[mask, 'ds_arrival_airport_long'] = df_data_prov.loc[mask, 'ds_arrival_airport_code'].map(coord_mapping)
+        
+        
+        
+        return df_data_prov
+    
+    except Exception as e:
+        print(f"Erreur dans prev_delay: {e}")
+        return np.nan
+
